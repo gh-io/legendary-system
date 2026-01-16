d83ee74ee89e34c6f75eec6f05ebcc45b1095f98
Seriki yakub <167559384+Web4application@users.noreply.github.com>
2026-01-17 00:42:45 +0100

    Update bash.sh

--- agentgateway/bash.sh
+++ agentgateway/bash.sh
@@ -1,1 +1,2 @@
 curl https://raw.githubusercontent.com/agentgateway/agentgateway/refs/heads/main/common/scripts/get-agentgateway | bash
+curl -L https://raw.githubusercontent.com/agentgateway/agentgateway/refs/heads/main/examples/basic/config.yaml -o config.yaml
